b70d0790f253b7eca7e9eb8cf4234f03c32bfa0b
김효은 <hyoeun@gmail.com>
2024-01-08 20:34:38 +0900

    babel 수정

--- .bablerc.js
+++ .bablerc.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-	presets : ['@bable/preset-env'],
-	plugins: ['@bable/plugin-transform-runtime']
+	presets : ['@babel/preset-env'],
+	plugins: ['@babel/plugin-transform-runtime']
 }
